0d6ac1f701b7c7712d9978c2dd38f70319965584
root <root@pvi.novalocal>
2020-12-02 13:56:33 +0000

    Fix legend display between species and proteins

--- app/js/network.js
+++ app/js/network.js
@@ -1,11 +1,11 @@
 d3.json("data/MOESM3_merge.json").then(function(input) {	
 
 	d3.json("data/MOESM1_merge2.json").then(function(data) {
-		network = {};
-		network.links = data.links.concat(input.links);
-		network.nodes = data.nodes.concat(input.nodes);
+            network = {};
+            network.links = data.links.concat(input.links);
+            network.nodes = data.nodes.concat(input.nodes);
 
-		var svg = d3.select("svg"),
+            var svg = d3.select("svg"),
 	    width = +svg.attr("width"),
 	    height = +svg.attr("height");
 
@@ -35,20 +35,19 @@ d3.json("data/MOESM3_merge.json").then(function(input) {
 	 
 		
 		function run(graph) {
-			graph.links.forEach(function(d){
+                    graph.links.forEach(function(d){
 			d.source = d.Source;    
 			d.target = d.Target;
-			});           
-
-			var link = g.append("g")
-	            .attr("class", "link")
-	            .selectAll("line")
-	            .data(graph.links)
-	            .enter().append("line")
+                    });           
+                    var link = g.append("g")
+	              .attr("class", "link")
+	              .selectAll("line")
+	              .data(graph.links)
+	              .enter().append("line")
 	            	.on('mouseout', fade(1))
 	            	.attr("stroke-width", function(d) { return Math.sqrt(d.value); });
 
-			var node = g.append("g")
+                     var node = g.append("g")
 		        .attr("class", "nodes")
 				.selectAll("g")
 		        .data(graph.nodes)
@@ -62,7 +61,7 @@ d3.json("data/MOESM3_merge.json").then(function(input) {
 			          .on("drag", dragged)
 			          .on("end", dragended));
 
-			/* Draw the respective pie chart for each node */
+		/* Draw the respective pie chart for each node */
 	        node.each(function (d) {
 	            NodePieBuilder.drawNodePie(d3.select(this), d.sp, {
 	                outerStrokeWidth: 1,
@@ -70,11 +69,11 @@ d3.json("data/MOESM3_merge.json").then(function(input) {
 	            });
 	        });
 
-	        drawLegend(colorClass);
+	        drawLegend(colorClass, "sp");
 
-			var label = g.append("g")
-			  .attr("class", "labels")
-			  .selectAll("text")
+		var label = g.append("g")
+		  .attr("class", "labels")
+		  .selectAll("text")
 			  .data(graph.nodes)
 			  .enter().append("text")
 			    .attr("class", "label")
@@ -83,14 +82,14 @@ d3.json("data/MOESM3_merge.json").then(function(input) {
 			    .attr("text-anchor", "middle")
 			    .text(function(d) { return d.id; });
 
-			simulation
-				.nodes(graph.nodes)
+		simulation
+			.nodes(graph.nodes)
 				.on("tick", ticked);
 
-			simulation.force("link")
+		simulation.force("link")
 				.links(graph.links);
 
-			function ticked() {
+		function ticked() {
 				link
 				    .attr("x1", function(d) { return d.source.x; })
 				    .attr("y1", function(d) { return d.source.y; })
@@ -105,32 +104,31 @@ d3.json("data/MOESM3_merge.json").then(function(input) {
                         return d.y;
                     });
 
-				label
-					.attr("x", function(d) { return d.x; })
-			        .attr("y", function (d) { return d.y; })
-			        .style("font-size", "12px").style("fill", "#000");
-			}
+                    label
+			.attr("x", function(d) { return d.x; })
+		        .attr("y", function (d) { return d.y; })
+		        .style("font-size", "12px").style("fill", "#000");
+		}
 			
-			const linkedByIndex = {};
-				graph.links.forEach(d => {
-				linkedByIndex[`${d.source.index},${d.target.index}`] = 1;
-			});
+		const linkedByIndex = {};
+		graph.links.forEach(d => {
+			linkedByIndex[`${d.source.index},${d.target.index}`] = 1;
+		});
 
-			function isConnected(a, b) {
-				return linkedByIndex[`${a.index},${b.index}`] || linkedByIndex[`${b.index},${a.index}`] || a.index === b.index;
-			}
+		function isConnected(a, b) {
+			return linkedByIndex[`${a.index},${b.index}`] || linkedByIndex[`${b.index},${a.index}`] || a.index === b.index;
+		}
 
-			function fade(opacity) {
-			    return d => {
-				    node.style('stroke-opacity', function (o) {
-				      const thisOpacity = isConnected(d, o) ? 1 : opacity;
-				      this.setAttribute('fill-opacity', thisOpacity);
-				      return thisOpacity;
-				    });
-
-			    	link.style('stroke-opacity', o => (o.source === d || o.target === d ? 1 : opacity));
-			    };
-			};
+		function fade(opacity) {
+		    return d => {
+			    node.style('stroke-opacity', function (o) {
+			      const thisOpacity = isConnected(d, o) ? 1 : opacity;
+			      this.setAttribute('fill-opacity', thisOpacity);
+			      return thisOpacity;
+			    });
+                            link.style('stroke-opacity', o => (o.source === d || o.target === d ? 1 : opacity));
+                    };
+		};
 		}
 
 		function dragstarted(d) {
@@ -166,12 +164,12 @@ d3.json("data/MOESM3_merge.json").then(function(input) {
 
 
 		$( ".form-control" ).change(function() {
-		  	
+		  	//console.log(this.value);
 		  	if (this.value == "pt"){
-		  		var node = d3.selectAll(".nodes g")
-			        .data(network.nodes);
-				/* Draw the respective pie chart for each node */
-		        node.each(function (d) {
+                            var node = d3.selectAll(".nodes g")
+			       .data(network.nodes);
+                            /* Draw the respective pie chart for each node */
+		            node.each(function (d) {
 		            var circle = d3.select(this).selectAll("circle");
 		            NodePieBuilder.drawNodePie(d3.select(this), d.group, {
 		                outerStrokeWidth: 1,
@@ -179,14 +177,16 @@ d3.json("data/MOESM3_merge.json").then(function(input) {
 		            });
 		            // Set pie position
 		            d3.selectAll("circle").attr("cx", function (d) {
-                        return d.x;
-                    })
-                    .attr("cy", function (d) {
-                        return d.y;
-                    });
-                    // Remove old circle
+                                return d.x;
+                            })
+                            .attr("cy", function (d) {
+                                return d.y;
+                            });
+                            // Remove old circle
 		            circle.remove();					
-		        });
+		          });
+                          svg.selectAll("g.legend").remove();
+                          drawLegend(colorClass, "pt");
 		  	}
 		  	if (this.value == "sp"){
 		  		var node = d3.selectAll(".nodes g")
@@ -206,15 +206,17 @@ d3.json("data/MOESM3_merge.json").then(function(input) {
                         return d.y;
                     });
                     // Remove old circle
-		            circle.remove();
-				});
-		  	}
+		    circle.remove();
 		});
-	});
+                svg.selectAll("g.legend").remove();
+                drawLegend(colorClass, "sp");
+             }
+         });
+     });
 });
 
 
-var colorClass = []
+var colorClass = [];
 colorClass['CIYVV'] = "#CDA458";
 colorClass['PPV'] = "#455059";
 colorClass['PRSV-P'] = "#F2D0A7";
@@ -227,9 +229,9 @@ colorClass['Arabidopsis'] = "#59DB9F";
 colorClass['Viral'] = "#4393c3";
 colorClass['Host'] = "#B2721A";
 
-function chooseColor(sp){
-	var color = colorClass;
-	return color[sp]
+function chooseColor(item, color=colorClass){
+	//var color = colorClass;
+	return color[item];
 }
 
 var DEFAULT_OPTIONS = {
@@ -334,7 +336,8 @@ var NodePieBuilder = {
     }
 };
 
-function drawLegend(colors){
+function drawLegend(colors, type){
+    console.log("Legende");
     var legend = d3.select("svg").append("g")
 	  .attr("class", "legend")
 	  .attr("x", d3.select("svg").attr("width") - 85)
@@ -343,20 +346,39 @@ function drawLegend(colors){
 	  .attr("width", 100);
 	var i = 0
 	for (var c in colors) {
-	    i = i + 20
-	    legend
-	       .append('rect')
-           .attr("x", d3.select("svg").attr("width") - 105)
-           .attr("y", i)
-           .attr("width", 20)
-           .attr("height", 20)
-           .attr("fill",chooseColor(c));
-
-        legend
-	      .append("text")
-		  .attr("x", d3.select("svg").attr("width") - 82)
-	      .attr("y", i +15 )
-		  .text(c);
-	}  
+            if (type == "sp"){
+               if (c != "Viral" && c != "Host"){
+	          i = i + 20
+	          legend
+	            .append('rect')
+                    .attr("x", d3.select("svg").attr("width") - 105)
+                    .attr("y", i)
+                    .attr("width", 20)
+                    .attr("height", 20)
+                    .attr("fill",chooseColor(c, colors));
+                  legend
+                    .append("text")
+                    .attr("x", d3.select("svg").attr("width") - 82)
+                    .attr("y", i +15 )
+                    .text(c);
+               }
+           }else{
+               if (c == "Viral" || c == "Host"){
+                  i = i + 20
+                  legend
+                     .append('rect')
+                     .attr("x", d3.select("svg").attr("width") - 105)
+                     .attr("y", i)
+                     .attr("width", 20)
+                     .attr("height", 20)
+                     .attr("fill",chooseColor(c, colors));
+                  legend
+	             .append("text")
+                     .attr("x", d3.select("svg").attr("width") - 82)
+	             .attr("y", i +15 )
+		     .text(c);
+               }
+          }
+     }  
 };
 
